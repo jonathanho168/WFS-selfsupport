2b08e97f7cb63a780f7d0e104d177dd3e27bd376
jonathanho168 <62679754+jonathanho168@users.noreply.github.com>
2024-03-27 16:36:38 -0400

    change DB name + execute in sql management studio

---
+++ WFSPortal/Databases/0_timesheetlisting.sql
@@ -0,0 +1,38 @@
+USE [EpicorHCMCMU2024]
+GO
+/****** Object:  StoredProcedure [dbo].[WFS_Manager_TimeSheetListing]    Script Date: 3/27/2024 4:18:53 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER PROCEDURE [dbo].[WFS_Manager_TimeSheetListing] 
+(
+@ManagerUsername nvarchar(100)
+)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+
+	-- forthekids\ = 12
+
+	SELECT PTGP.PersonTimeGroupPeriodGUID
+	  FROM tPersonTimeGroupPeriod ptgp with (nolock)
+	  JOIN tTimeGroupPeriod tgp with (nolock) on ptgp.TimeGroupPeriodGUID = tgp.TimeGroupPeriodGUID
+	  JOIN tperson p with (nolock) on ptgp.PersonGUID = p.PersonGUID
+	  JOIN vWesleyActiveSuperview was with (nolock) on ptgp.PersonGUID = was.PersonGUID and ptgp.PositionCode = was.PositionCode
+	  JOIN USysUser man with (nolock) on was.SupervisorPersonGUID = man.PersonGUID
+	 WHERE ptgp.SubmittedFlag = 1 and ptgp.ApprovedFlag = 1
+	   AND tgp.ClosedFlag = 0
+	   AND man.username = @ManagerUsername
+
+END
+
