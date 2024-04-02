915823be17c6fa23a3b1c617ee485c918747d015
jonathanho168 <62679754+jonathanho168@users.noreply.github.com>
2024-04-02 15:54:44 -0400

    add mvc

--- WFSPortal/Program.cs
+++ WFSPortal/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
