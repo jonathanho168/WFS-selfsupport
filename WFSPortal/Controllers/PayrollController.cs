f13b59322ddb53d73bc69dd4d56937378df15176
jonathanho168 <62679754+jonathanho168@users.noreply.github.com>
2024-04-02 15:44:14 -0400

    controller for retrieving time sheet listings for signed-in manager

---
+++ WFSPortal/Controllers/PayrollController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+using WFSPortal.Data;
+using WFSPortal.Models;
+
+namespace WFSPortal.Controllers
+{
+    [Authorize] // Ensure only authorized (signed-in) users can access this controller
+    public class PayrollController : Controller
+    {
+        private readonly Epicorhcmcmu2024Context _context;
+
+        public PayrollController(Epicorhcmcmu2024Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Assuming the manager's username is stored as the user's name
+            var managerUsername = User.Identity.Name;
+
+            // Retrieve time sheet listings for the signed-in manager
+            var listings = await _context.GetTimeSheetListingsAsync(managerUsername);
+
+            // Pass the listings to the view
+            return View("~/Views/Payroll/Index.cshtml", listings);
+        }
+    }
+}
